02ba558e8d5ecfa75d90ef8a7a15c14fc7cb832e
Victor.Sergeyev <victor17011998@yandex.ru>
2022-09-22 19:55:58 +0400

    fixed: today route

--- src/Http/Routes/LogViewerRoute.php
+++ src/Http/Routes/LogViewerRoute.php
@@ -27,13 +27,13 @@ class LogViewerRoute extends RouteRegistrar
     {
         $attributes = (array) config('log-viewer.route.attributes');
 
-        $this->group($attributes, function() {
+        $this->group($attributes, function () {
             $this->name('log-viewer::')->group(function () {
                 // log-viewer::dashboard
                 $this->get('/', [LogViewerController::class, 'index'])->name('dashboard');
 
                 // log-viewer::today
-                $this->get('today', 'LogViewerController@today')->name('today');
+                $this->get('today', [LogViewerController::class, 'today'])->name('today');
 
                 $this->mapLogsRoutes();
             });
@@ -47,27 +47,27 @@ class LogViewerRoute extends RouteRegistrar
     {
         $this->prefix('logs')->name('logs.')->group(function () {
             $this->get('/', [LogViewerController::class, 'listLogs'])
-                 ->name('list'); // log-viewer::logs.list
+                ->name('list'); // log-viewer::logs.list
 
             $this->delete('delete', [LogViewerController::class, 'delete'])
-                 ->name('delete'); // log-viewer::logs.delete
+                ->name('delete'); // log-viewer::logs.delete
 
             $this->prefix('{prefix}')->group(function () {
                 $this->prefix('{date}')->group(function () {
                     $this->get('/', [LogViewerController::class, 'show'])
-                         ->name('show'); // log-viewer::logs.show
+                        ->name('show'); // log-viewer::logs.show
 
                     $this->get('download', [LogViewerController::class, 'download'])
-                         ->name('download'); // log-viewer::logs.download
+                        ->name('download'); // log-viewer::logs.download
 
                     $this->get('{level}', [LogViewerController::class, 'showByLevel'])
-                         ->name('filter'); // log-viewer::logs.filter
+                        ->name('filter'); // log-viewer::logs.filter
 
                     $this->get('{level}/search', [LogViewerController::class, 'search'])
-                         ->name('search'); // log-viewer::logs.search
+                        ->name('search'); // log-viewer::logs.search
 
                     $this->get('{level}/similar', [LogViewerController::class, 'showSimilar'])
-                         ->name('similar'); // log-viewer::logs.similar
+                        ->name('similar'); // log-viewer::logs.similar
                 });
             });
         });
